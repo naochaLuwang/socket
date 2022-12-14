a894ff52513795006d430562b17931f2363e2898
naochaLuwang <somoarambam123@gmail.com>
2022-12-14 12:12:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
+});
+
+server.listen(4000, () => {
+  console.log("Server is running");
+});
